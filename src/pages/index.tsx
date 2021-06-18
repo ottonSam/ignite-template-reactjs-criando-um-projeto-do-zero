6983f6876465cabbfb25087840d3fe7783d70498
ottonsam <ottonsam@gmail.com>
2021-06-17 22:43:06 -0300

    Buscando os dados do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps, GetStaticProps } from 'next';
+
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,25 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home({postsPagination}: HomeProps) {
+    console.log(postsPagination)
+    return (
+      <h1>opa</h1>
+    )
+ }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+ export const getStaticProps: GetServerSideProps = async () => {
+    const prismic = getPrismicClient();
+    const postsResponse = await prismic.query([
+      Prismic.Predicates.at('document.type', 'post')
+    ], {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 20,
+    });
+    console.log(postsResponse);
 
-//   // TODO
-// };
+    return {
+      props: {postsResponse}
+    };
+
+ };
